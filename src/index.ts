037e504610b4bc378e320fc1996bfd410f38ad66
actuallylost <pandagaminggr@gmail.com>
2019-07-23 19:46:39 +0300

    Fixed stuff here

--- src/index.ts
+++ src/index.ts
@@ -57,25 +57,6 @@ client.on("message", (message) => {
     case "clear": {
       clear(client, message, args);
     }
-    case "info": {
-      const infoEmbed = new Discord.RichEmbed()
-        .setTitle("User Information")
-        .setColor("#4de8e8")
-        .addField("User:", message.author.tag)
-        .addField("ID:", message.author.id)
-        .addField(
-          "Roles:",
-          message.member.roles.map((r) => `<@${r.id}>`).join(", ")
-        )
-        .addField("Created at:", message.author.createdAt)
-        .addField("Joined at", message.member.joinedAt)
-        .setFooter(
-          `Requested by ${message.author.tag}`,
-          message.author.avatarURL
-        )
-        .setTimestamp();
-      return message.channel.send(infoEmbed);
-    }
     case "botinfo": {
       const botinfoEmbed = new Discord.RichEmbed()
         .setTitle("Bot Information")
@@ -122,36 +103,6 @@ client.on("message", (message) => {
       } catch (err) {
         message.channel.send(`\`ERROR\` \`\`\`xl\n${clean(err)}\n\`\`\``);
       }
-    }
-    case "hug": {
-      if (!args[1]) {
-        const hugErrorEmbed1 = new Discord.RichEmbed()
-          .setTitle("Hug Error")
-          .setColor("#d91818")
-          .setDescription(
-            ":x: Oops! You didn't input an id or tagged a member to hug. Format is `+hug <user>`."
-          );
-        return message.channel.send(hugErrorEmbed1);
-      }
-      const userToHug = message.mentions.users.first();
-      if (userToHug == null) {
-        const hugErrorEmbed2 = new Discord.RichEmbed()
-          .setTitle("Hug Error")
-          .setColor("#d91818")
-          .setDescription(
-            ":x: Oops! That user doesn't exist, maybe you typed something wrong? Format is `+hug <user>`."
-          );
-        return message.channel.send(hugErrorEmbed2);
-      }
-      const hugEmbed = new Discord.RichEmbed()
-        .setTitle(`Hugged ${userToHug.username} :3`)
-        .setColor("#0c1db3")
-        .setDescription(
-          `Awh, ${userToHug} was just hugged by ${
-            message.author.username
-          }. How sweet :3`
-        );
-      return message.channel.send(hugEmbed);
     }
     case "kick": {
       if (!message.member.hasPermission("KICK_MEMBERS")) {
